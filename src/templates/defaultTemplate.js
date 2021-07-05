9d1853592dc4ad6f2e16f1bff4c17df311fb1975
deokgoo <kkddgg1001@gmail.com>
2021-07-05 22:56:10 +0900

    fix: template design

--- src/templates/defaultTemplate.js
+++ src/templates/defaultTemplate.js
@@ -11,18 +11,22 @@ export default function BlogTemplate({children, pageContext}) {
 
   return (
     <div className="blog-post-container">
-      <Seo title={title} description={description} />
-      <div className="blog-post-articles" role="link" tabIndex={0} onClick={_ => window.location.href='/'} onKeyPress={_ => window.location.href='/'}>
-        <span>
-          All Articles
-        </span>
-      </div>
-      <div className="blog-post">
-        <h1 className="blog-post__title">{title}</h1>
-        <small className="blog-post__date">({date})</small>
-        {children}
-        <div className="blog-post-footer">
-          <hr/>
+      <div className="blog-post-wrapper">
+        <Seo title={title} description={description} />
+        <div className="blog-post-articles" role="link" tabIndex={0} onClick={_ => window.location.href='/'} onKeyPress={_ => window.location.href='/'}>
+          <div className="blog-post-articles__btn">
+            All Articles
+          </div>
+        </div>
+        <div className="blog-post-content">
+          <h1 className="blog-post-content__title">{title}</h1>
+          <h2 className="blog-post-content__date">({date})</h2>
+          <div className="blog-post-content__content">
+            {children}
+          </div>
+          <div className="blog-post-footer">
+            <hr/>
+          </div>
         </div>
       </div>
     </div>
