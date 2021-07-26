1137293c3dde75d8c73c6fbe86561d4ae984c8dc
deokgoo <kkddgg1001@gmail.com>
2021-07-27 01:48:10 +0900

    feature: programmers sort 42746

--- src/components/seo/seo.tsx
+++ src/components/seo/seo.tsx
@@ -4,6 +4,7 @@ import { SeoProps } from './type';
 
 const defaultMeta = [];
 const blogTitle = 'duck blog';
+const siteUrl = 'https://duck-blog.netlify.app';
 
 const Seo = ({
   lang = 'kr',
@@ -13,53 +14,59 @@ const Seo = ({
   img,
   description }: SeoProps) => {
   return (
-    <Helmet
-      htmlAttributes={{ lang }}
-      title={title??blogTitle}
-      titleTemplate={title ? `%s | ${blogTitle}` : blogTitle}
-      meta={[
-        {
-          name: `description`,
-          content: description,
-        },
-        {
-          property: `og:title`,
-          content: title??blogTitle,
-        },
-        {
-          property: `og:description`,
-          content: description,
-        },
-        {
-          property: `og:type`,
-          content: `website`,
-        },
-        {
-          property: `og:image`,
-          content: img,
-        },
-        {
-          name: `twitter:card`,
-          content: `summary`,
-        },
-        {
-          name: `twitter:creator`,
-          content: author,
-        },
-        {
-          name: `twitter:title`,
-          content: title??blogTitle,
-        },
-        {
-          name: `twitter:description`,
-          content: description,
-        },
-        {
-          name: `naver-site-verification`,
-          content: `da893f74c3322cdabd86b8e116f4d0f88b3c0cbf`
-        }
-      ].concat(meta)}
-    />
+    <>
+    
+      <Helmet>
+        <meta name="robots" content="noindex"></meta>
+      </Helmet>
+      <Helmet
+        htmlAttributes={{ lang }}
+        title={title??blogTitle}
+        titleTemplate={title ? `%s | ${blogTitle}` : blogTitle}
+        meta={[
+          {
+            name: `description`,
+            content: description,
+          },
+          {
+            property: `og:title`,
+            content: title??blogTitle,
+          },
+          {
+            property: `og:description`,
+            content: description,
+          },
+          {
+            property: `og:type`,
+            content: `website`,
+          },
+          {
+            property: `og:image`,
+            content: img,
+          },
+          {
+            name: `twitter:card`,
+            content: `summary`,
+          },
+          {
+            name: `twitter:creator`,
+            content: author,
+          },
+          {
+            name: `twitter:title`,
+            content: title??blogTitle,
+          },
+          {
+            name: `twitter:description`,
+            content: description,
+          },
+          {
+            name: `naver-site-verification`,
+            content: `da893f74c3322cdabd86b8e116f4d0f88b3c0cbf`
+          }
+        ].concat(meta)}
+      />
+    </>
   )
 }
 
