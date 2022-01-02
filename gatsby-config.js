d123ea5bb353235724b59706eb6c7cd79d58cc69
deokgoo <kkddgg1001@gmail.com>
2022-01-02 22:53:58 +0900

    fix: modify image

--- gatsby-config.js
+++ gatsby-config.js
@@ -141,7 +141,7 @@ module.exports = {
       resolve: 'gatsby-source-filesystem',
       options: {
         name: 'images',
-        path: `${__dirname}/src/pages`,
+        path: `${__dirname}/src/images`,
       },
       __key: 'images',
     },
